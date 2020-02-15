22f4fe611d76cf277bca358448a9ac529b3e6b28
arllainm <arllain@gmail.com>
2020-02-15 05:39:02 -0300

    roteamento no React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello React</h1>
+    <div className="App">
+      <Routes />
     </div>
   );
 }
